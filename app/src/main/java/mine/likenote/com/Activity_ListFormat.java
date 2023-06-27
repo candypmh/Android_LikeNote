a7a06c4013e138010b89d930f6af78622d300aba
Minep <pmh@ge.kornu.ac.kr>
2023-06-27 14:10:22 +0900

    2023-06-27 commit 어제 잘못한부분 수정

--- app/src/main/java/mine/likenote/com/Activity_ListFormat.java
+++ app/src/main/java/mine/likenote/com/Activity_ListFormat.java
@@ -15,7 +15,8 @@ public class Activity_ListFormat extends AppCompatActivity {
 
     //Nav연동해서 ListActivity에 얹을 프래그먼트 구현
     FragmentManager fragmentManager;
-    FragmentTransaction fragmentTransaction;
+    //FragmentTransaction fragmentTransaction;
+
 
 
 
@@ -26,7 +27,8 @@ public class Activity_ListFormat extends AppCompatActivity {
 
         //프래그먼트 구현(액티비티 위에서 프래그먼트 관리)
         fragmentManager = getSupportFragmentManager(); //프래그먼트 관리. 프래그먼트를 추가, 제거, 교체 및 상호작용하는 데 사용
-        fragmentTransaction = fragmentManager.beginTransaction(); //beginTransaction 메서드 통해서 fragmentTransaction 객체 생성하고
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); //beginTransaction 메서드 통해서 fragmentTransaction 객체 생성하고
+
                                                                   //이 객체로 프래그먼트를 추가, 제거 또는 교체할 수 있음
         fragmentTransaction.replace(R.id.listFrame, new Fragment_List());  //fragment.java 만들어 줌. 이렇게 new하면 새로고침됨
                                                                            //replace메서드 통해 프래그먼트 바꾸기 가능
@@ -53,6 +55,8 @@ public class Activity_ListFormat extends AppCompatActivity {
 
     //changeFragment 메서드 만들기
     private void changeFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); //beginTransaction 메서드 통해서 fragmentTransaction 객체 생성하고
+        //이 객체로 프래그먼트를 추가, 제거 또는 교체할 수 있음
         fragmentTransaction.replace(R.id.listFrame, fragment);
         fragmentTransaction.commit();
     }
