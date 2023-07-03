f73461b8ce1e1fe8e64a0a8dead885fa7f70a0ff
Minep <pmh@ge.kornu.ac.kr>
2023-07-04 01:54:43 +0900

    20230704 fragment detail xml및 java작업

--- app/src/main/java/mine/likenote/com/Activity_ListFormat.java
+++ app/src/main/java/mine/likenote/com/Activity_ListFormat.java
@@ -28,7 +28,6 @@ public class Activity_ListFormat extends AppCompatActivity {
         //프래그먼트 구현(액티비티 위에서 프래그먼트 관리)
         fragmentManager = getSupportFragmentManager(); //프래그먼트 관리. 프래그먼트를 추가, 제거, 교체 및 상호작용하는 데 사용
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); //beginTransaction 메서드 통해서 fragmentTransaction 객체 생성하고
-
                                                                   //이 객체로 프래그먼트를 추가, 제거 또는 교체할 수 있음
         fragmentTransaction.replace(R.id.listFrame, new Fragment_List());  //fragment.java 만들어 줌. 이렇게 new하면 새로고침됨
                                                                            //replace메서드 통해 프래그먼트 바꾸기 가능
